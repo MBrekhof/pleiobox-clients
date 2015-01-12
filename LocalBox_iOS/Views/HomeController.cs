e487bfe4d7ce8dfdb72680976f8104c5b2d5a609
Stefan <stefan.collette@clockwork.nl>
2015-01-12 12:52:58 +0100

    [iOS] minor bug fix

--- LocalBox_iOS/Views/HomeController.cs
+++ LocalBox_iOS/Views/HomeController.cs
@@ -225,6 +225,8 @@ namespace LocalBox_iOS.Views
         }
 
 		public void InitialiseMenuAfterRegistration(){
+
+
 			UpdateMaster(new MenuViewController(this));
 			RemoveDetail();
 			kleurenBalk.BackgroundColor = _defaultColor;
@@ -240,11 +242,115 @@ namespace LocalBox_iOS.Views
 				result = await BusinessLayer.Instance.Authenticate (localBoxToAdd);
 
 				if (result) {
-					LocalBox_iOS.Helpers.CryptoHelper.ValidateKeyPresence (localBoxToAdd, InitialiseMenuAfterRegistration);
+					ValidateKeyPresence (localBoxToAdd, InitialiseMenuAfterRegistration);
 					AppDelegate.localBoxToRegister = null;
 				}
 			});
 		}
+
+
+
+		public void ValidateKeyPresence(LocalBox localBox, Action onCompletion) {
+			if (localBox.HasCryptoKeys)
+			{
+				if (!localBox.HasPassPhrase)
+				{
+					ShowPassphraseForExistingKey(localBox, onCompletion);
+				}
+			}
+			else
+			{
+				CreateNewKeyPair(localBox, onCompletion);
+			}
+		}
+
+
+		private void ShowPassphraseForExistingKey(LocalBox localBox, Action onCompletion, bool repeat = false) {
+			string message = string.Empty;
+			if (repeat)
+			{
+				message = "Verkeerde passphrase opgegeven, probeer het opnieuw";
+			}
+			else
+			{
+				message = "Bij deze LocalBox is al een sleutel aanwezig, geef de passphrase op van deze sleutel";
+			}
+
+			UIAlertView createPassPhrase = new UIAlertView("Passphrase", message, null, "Annuleer", "Ok");
+			createPassPhrase.AlertViewStyle = UIAlertViewStyle.SecureTextInput;
+			createPassPhrase.GetTextField(0).Placeholder = "Passphrase";
+
+			createPassPhrase.Clicked += async (object s, UIButtonEventArgs args) => {
+				if(args.ButtonIndex == 0) {
+					DataLayer.Instance.DeleteLocalBox(localBox.Id);
+					if(onCompletion != null) {
+						onCompletion();
+					}
+				}else if(args.ButtonIndex == 1) {
+					if(!await BusinessLayer.Instance.ValidatePassPhrase(localBox ,createPassPhrase.GetTextField(0).Text)) {
+						ShowPassphraseForExistingKey(localBox, onCompletion, true);
+					} else {
+						if(onCompletion != null) {
+							onCompletion();
+						}
+					}
+				}
+			};
+			createPassPhrase.Show();
+		}
+
+		private void CreateNewKeyPair(LocalBox localBox, Action onCompletion, bool repeat = false)
+		{
+			string message = repeat ? "De opgegeven passphrases komen niet overeen, probeer het nog eens" : "U moet een passphrase instellen voordat u deze LocalBox kunt gebruiken";
+
+			UIAlertView createPassPhrase = new UIAlertView("Passphrase", message, null, "Annuleer", "Ok");
+			createPassPhrase.AlertViewStyle = UIAlertViewStyle.LoginAndPasswordInput;
+			var firstField = createPassPhrase.GetTextField(0);
+			var secondField = createPassPhrase.GetTextField(1);
+
+			firstField.Placeholder = "Passphrase";
+			secondField.Placeholder = "Bevestig Passphrase";
+
+			firstField.AutocorrectionType = UITextAutocorrectionType.No;
+			firstField.SecureTextEntry = true;
+
+
+			createPassPhrase.ShouldEnableFirstOtherButton = (av)=> {
+				return av.GetTextField(0).Text.Length > 0 && av.GetTextField(0).Text.Equals(av.GetTextField(1).Text);
+			};
+
+			createPassPhrase.Clicked += async (object s, UIButtonEventArgs args) => {
+
+
+				if (args.ButtonIndex == 0) {
+					DataLayer.Instance.DeleteLocalBox (localBox.Id);
+					if (onCompletion != null) {
+						onCompletion ();
+					}
+				} else if (args.ButtonIndex == 1) {
+					if (firstField.Text.Equals (secondField.Text)) {
+	
+						DialogHelper.ShowBlockingProgressDialog ("Beveiliging", "Bezig met het creëren van sleutels. Dit kan enige tijd in beslag nemen.");
+						bool result = await BusinessLayer.Instance.SetPublicAndPrivateKey (localBox, firstField.Text); 
+						if (!result) {
+							DataLayer.Instance.DeleteLocalBox (localBox.Id);
+							DialogHelper.ShowErrorDialog ("Fout", string.Format ("Er is een fout opgetreden bij het toevoegen van de LocalBox {0}. De LocalBox is niet toegevoegd", localBox.Name));
+						} 
+						if (onCompletion != null) {
+							onCompletion ();
+						}
+						DialogHelper.HideBlockingProgressDialog ();
+
+					} else {
+						CreateNewKeyPair (localBox, onCompletion, true);
+					}
+					DialogHelper.HideProgressDialog ();
+				}
+			};
+			createPassPhrase.Show();
+		}
+
+
 
 
 
